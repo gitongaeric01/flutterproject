7e977833df21a1d826b52be1e765c9100ad3e5db
Your Name <gitongaeric01@gmail.com>
2021-04-23 05:12:49 +0300

    initial flutter app

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/widgets.dart';
+
+import 'package:flutter/material.dart';
+
+main() {}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("my list"),
+        ),
+      ),
+    );
+  }
+}
